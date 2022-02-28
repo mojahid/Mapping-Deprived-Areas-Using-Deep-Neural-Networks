c76227bac102acc4127ed0a22ef5c22aa3ed2005
breardon7 <70675472+breardon7@users.noreply.github.com>
2022-02-27 22:31:20 -0500

    SVD

--- Contextual_Features_Preprocessing_and_Modeling.py
+++ Contextual_Features_Preprocessing_and_Modeling.py
@@ -10,6 +10,8 @@ from sklearn.metrics import accuracy_score, roc_auc_score
 from sklearn.metrics import classification_report
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import TruncatedSVD
+from sklearn.model_selection import cross_val_score, RepeatedStratifiedKFold
+from sklearn.pipeline import Pipeline
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -41,6 +43,57 @@ svd = TruncatedSVD(n_components=25, n_iter=7, random_state=42) # define transfor
 svd.fit(X) # prepare transform on dataset
 X_transformed = svd.transform(X) # apply transform to dataset
 print('post-SVD shape: ', X_transformed.shape)
+
+# SVD analysis
+H = np.matmul(X.T, X)
+
+u, s, v = np.linalg.svd(H) #, full_matrices=True)
+#print('Singular values of original = ', s)
+singular = s[:30]
+var_list = []
+for value in singular:
+    var_list =+ singular**2/np.sum(singular**2)
+print('------------variance list------------')
+print(singular[2])
+print(var_list[2])
+x_axis = np.arange(30)
+plt.plot(x_axis, var_list)
+plt.xlabel('Number of Components')
+plt.ylabel('Prop. of Variance Explained')
+plt.title('SVD: Cumulative Explained Variance')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
+# Cross Validating TruncatedSVD paired with Logistic Regression model
+def get_models():
+    models = dict()
+    for i in range(1,140):
+        steps = [('svd', TruncatedSVD(n_components=i)), ('m', LogisticRegression())]
+        models[str(i)] = Pipeline(steps=steps)
+    return models
+# evaluate a give model using cross-validation
+def evaluate_model(model, X, y):
+    cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+    scores = cross_val_score(model, X, y, scoring='accuracy', cv=cv, n_jobs=-1, error_score='raise')
+    return scores
+# get the models to evaluate
+models = get_models()
+# evaluate the models and store results
+results, names = list(), list()
+for name, model in models.items():
+    scores = evaluate_model(model, X, y)
+    results.append(scores)
+    names.append(name)
+print('>%s %.3f (%.3f)' % (name, np.mean(scores), np.std(scores)))
+# plot model performance for comparison
+plt.boxplot(results, labels=names, showmeans=True)
+plt.xlabel('Number of Components')
+plt.ylabel('Accuracy')
+plt.title('Truncated SVD: Number of Component Analysis')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
 
 # train test split
 X_train, X_test, y_train, y_test = train_test_split(X_transformed, y, test_size=0.2, random_state=0)
