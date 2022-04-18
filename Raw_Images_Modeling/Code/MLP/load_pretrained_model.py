65dcef65f9043c5b88db09ff4774283d71a43940
mojahid <mojahid_dool@hotmail.com>
2022-04-18 02:56:30 -0400

    updated pretrained model code

--- Raw_Images_Modeling/Code/MLP/load_pretrained_model.py
+++ Raw_Images_Modeling/Code/MLP/load_pretrained_model.py
@@ -6,12 +6,20 @@ import cv2
 from keras import layers, losses
 from PIL import Image
 
-
 MODEL = 'encoder_model'
+MODEL2 = 'pretrained_encoder_model'
 data_dir = r'../data/test/png'
 models = r'../code/ae_models/{}.h5'
+model_path=r'../code/ae_models/'
 
-decoder_model = tf.keras.models.load_model(models.format(MODEL))
-
+model = tf.keras.models.load_model(models.format(MODEL))
+model.add(tf.keras.layers.BatchNormalization(name='batch_normalization_3'))
+model.add(tf.keras.layers.Dense(2, activation='softmax',name='dense_4'))
+#print(model.layers.pop())
 # Check its architecture
-decoder_model.summary()
+model.summary()
+
+model.save(model_path+'model_pretrained_encoder_model.h5')
+
+model2 = tf.keras.models.load_model(models.format(MODEL2))
+model2.summary()
