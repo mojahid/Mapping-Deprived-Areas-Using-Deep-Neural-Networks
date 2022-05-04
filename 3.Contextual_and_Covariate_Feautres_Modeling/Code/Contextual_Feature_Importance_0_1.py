2e3f079c6ca787d2d93972f1ac6197e79e79f977
Jcolt2997 <60163434+Jcolt2997@users.noreply.github.com>
2022-05-04 11:35:59 -0400

    Update Contextual_Feature_Importance_0_1.py

--- 3.Contextual_and_Covariate_Feautres_Modeling/Code/Contextual_Feature_Importance_0_1.py
+++ 3.Contextual_and_Covariate_Feautres_Modeling/Code/Contextual_Feature_Importance_0_1.py
@@ -1288,13 +1288,7 @@ best.to_csv(root / '3.Contextual_and_Covariate_Feautres_Modeling' / 'feature_sel
 #best.to_csv(path_or_buf='feature_selection/Contextual/' + 'Contextual_Features_Ranking.csv',index=False)
 
 
-# In[74]:
 
-
-best
-
-
-# In[ ]:
 
 
 
